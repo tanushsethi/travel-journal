dbcdc9abbe4c9f481b685535bc186e68455421cc
Tanush Sethi <tanushsethi55@gmail.com>
2022-09-09 22:29:44 +0530

    data and css update

--- src/components/RightCard.js
+++ src/components/RightCard.js
@@ -3,7 +3,20 @@ import React from "react";
 function RightCard(props) {
     return (
         <div className="rightInner">
-            
+            <div className="div1">
+                <img src="images/marker.png" alt="" className="marker"/>
+                <span className="">{props.location}</span>
+                <a href={props.googleMapsUrl}>View on Google Mans</a>
+            </div>
+            <div className="div2">
+                <h1>{props.title}</h1>
+            </div>
+            <div className="div3">
+                <p>{props.startDate} - {props.endDate}</p>
+            </div>
+            <div className="div4">
+                <p>{props.description}</p>
+            </div>
         </div>
     );
 }
